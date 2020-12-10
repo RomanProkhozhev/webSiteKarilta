4fd2505c970aa2058e11fc2ca4258f19ccdbedc3
Roman Prokhozhev <prohozhev12@gmail.com>
2020-12-10 13:59:54 +0200

    Pages links fix

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'websitekarilta' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'roman' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'roman' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
