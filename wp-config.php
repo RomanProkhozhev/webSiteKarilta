41c2faad837f69743160ef1a8e2042dcfa10cbc4
RomanProkhozhev <prohozhev12@gmail.com>
2020-12-10 15:55:30 +0200

    wp-config.php -File modified

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'websitekarilta' );
 
 /** MySQL database username */
-define( 'DB_USER', 'roman' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'roman' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
